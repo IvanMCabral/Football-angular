64b9f8ceea29cbaf08289f3df785b88bbf0e3515
ichu_user <ichu_@example.com>
2026-07-24 10:23:29 -0300

    remove duplicate toast implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { ToastContainerComponent } from './shared/components/toast-container/toast-container.component';
-import { ToastComponent } from './shared/components/toast/toast.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, ToastContainerComponent, ToastComponent],
+  imports: [RouterOutlet, NavbarComponent, ToastContainerComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
